f1a5c77b18d97616c9f2584dcdbf463a2b0fd2e1
Renan Pollo Benelli <111061315+Renan1Pollo@users.noreply.github.com>
2023-01-16 23:58:48 -0300

    att commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,37 @@
 const inputTarefa = document.querySelector('.input-tarefa');
 const lista = document.querySelector('.tarefas');
 
+inputTarefa.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13){
+        if (!inputTarefa.value)
+        return;
+        criarTarefa(inputTarefa.value);
+    }
+}); // capturando a tecla que foi pressionada
+
+function limparInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criarBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'apagar';
+    li.appendChild(botaoApagar);
+}
+
 function criarTarefa(textinput) {
     const li = document.createElement('li');
-    
+
     li.innerHTML = textinput;
     lista.appendChild(li);
+    limparInput();
 }
 
 function addTarefa() {
     if (!inputTarefa.value)
-    return;
+        return;
     criarTarefa(inputTarefa.value);
+    criarBotaoApagar(li);
 }
