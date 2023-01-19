8fe33cdd1636928a757a4535089c4d303d103358
Renan Pollo Benelli <Renansk18@outlook.com>
2023-01-19 10:23:40 -0300

    commit att

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,9 @@ const inputTarefa = document.querySelector('.input-tarefa');
 const lista = document.querySelector('.tarefas');
 
 inputTarefa.addEventListener('keypress', function (e) {
-    if (e.keyCode === 13){
+    if (e.keyCode === 13) {
         if (!inputTarefa.value)
-        return;
+            return;
         criarTarefa(inputTarefa.value);
     }
 }); // capturando a tecla que foi pressionada
@@ -17,21 +17,16 @@ function limparInput() {
 function criarTarefa(textinput) {
     const li = document.createElement('li');
 
-    const span =document.createElement('span');
+    const span = document.createElement('span');
     span.classList.add('textoTarefa');
     span.innerHTML = textinput;
 
     const div = document.createElement('div');
 
-    const btnEditor = document.createElement('button');
-    btnEditor.classList.add('btn-acao');
-    btnEditor.innerHTML = `<span class="edit">✎</span>`;
-
     const btnExcluir = document.createElement('button');
     btnExcluir.classList.add('btn-acao');
     btnExcluir.innerHTML = `<span class="trash">🗑</span>`;
 
-    div.appendChild(btnEditor);
     div.appendChild(btnExcluir);
 
     li.appendChild(span);
@@ -45,6 +40,15 @@ function addTarefa() {
     if (!inputTarefa.value)
         return;
     criarTarefa(inputTarefa.value);
-    criarBotaoApagar(li);
 }
 
+document.addEventListener('click', function (e) {
+    const el = e.target;
+    if (el.classList.contains('trash')) {
+        let confirm = window.confirm('Tem certeza que deseja excluir ?');
+        if (confirm == true) {
+            let btnApagar = document.querySelector('.tarefas div');
+            btnApagar.parentElement.remove();
+        }
+    }
+});
